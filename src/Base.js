dbc15c6feea0d19891d490cabbbe76e5e3a10fd9
Jonathan Santerre <santerrejonathan@gmail.com>
2017-02-08 16:48:42 -0500

    remove unused comment

--- src/Base.js
+++ src/Base.js
@@ -1,10 +1,6 @@
 // @flow
 import React from 'react';
 
-if (process.env.NODE_ENV === 'production') {
-  console.log("production!!")
-}
-
 class Base extends React.Component {
   render() {
     if (!this.props) {
